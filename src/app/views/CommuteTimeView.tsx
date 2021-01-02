8e8295cb5225b13068d4a5aef70ac1fe3d171e9d
Martin Marinov <martintzvetomirov@gmail.com>
2021-01-02 13:25:55 +0000

    Micro optimize commute time render with profiling

--- src/app/views/CommuteTimeView.tsx
+++ src/app/views/CommuteTimeView.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { TileInfo } from "../map/CanvasOverlay";
-import { CompiledModel } from "../model/CompiledModel";
+import { CompiledModel, ModelPoi } from "../model/CompiledModel";
 import DrawLib from "../utils/DrawLib";
 import { CompiledModelOverlay } from "../model/CompiledModelOverlay";
 import { ModelMapMarker } from "../pages/city/ModelMapMarker";
@@ -36,40 +36,56 @@ const RENDER_SET_UP = [
   },
 ];
 
-const sq = (x: number) => x * x;
-
-const boundsFilledScreenCompletely = (
-  bounds: LatLngBounds,
+const calculateBoundsForPoi = (
+  p: ModelPoi,
   tileBounds: LatLngBounds
-) => {
-  // First let's do cheap rectangle intersection
-  if (!bounds.contains(tileBounds)) {
-    return false;
-  }
-
-  // Now let's do circle radius check
-
-  // Poi is an inscribed circle
-  const radBounds =
-    Math.min(
-      bounds.getEast() - bounds.getWest(),
-      bounds.getNorth() - bounds.getSouth()
-    ) / 2;
-  // Tile is circumscribed circle
-  const tileRad =
-    Math.sqrt(
-      sq(tileBounds.getEast() - tileBounds.getWest()) +
-        sq(tileBounds.getNorth() - tileBounds.getSouth())
-    ) / 2;
-
-  const boundsCenter = bounds.getCenter();
-  const tileCenter = tileBounds.getCenter();
-  const dist = Math.sqrt(
-    sq(boundsCenter.lat - tileCenter.lat) +
-      sq(boundsCenter.lng - tileCenter.lng)
+): (LatLngBounds | undefined)[] => {
+  const renderBounds: (LatLngBounds | undefined)[] = Array(
+    RENDER_SET_UP.length
   );
 
-  return dist < radBounds - tileRad;
+  RENDER_SET_UP.forEach((renderer, rid) => {
+    if (p.travelTime >= renderer.maxTravelTime) {
+      // Unreachable anymore, next time maxTravelTime will get even smaller
+      return;
+    }
+    const walkingTimeUnconstrained = renderer.maxTravelTime - p.travelTime;
+    const walkingTime = MAX_WALKING_TIME(walkingTimeUnconstrained);
+    const newBounds = p.bounds
+      .getCenter()
+      .toBounds(2 * timeToDistance(walkingTime));
+    if (!newBounds.intersects(tileBounds)) {
+      // The point is no longer renderable within this tile
+      // over the next passes as maxTravelTime gets lower
+      // there will be no more chances to render this tile
+      return;
+    }
+    renderBounds[rid] = newBounds;
+  });
+
+  // Second pass, remove bounds that overlap with next pass
+  // as these are no-ops
+  for (let rid = 0; rid < RENDER_SET_UP.length - 1; rid++) {
+    const bounds = renderBounds[rid];
+    const nextBounds = renderBounds[rid + 1];
+    if (
+      bounds !== undefined &&
+      nextBounds !== undefined &&
+      nextBounds.contains(bounds)
+    ) {
+      renderBounds[rid] = undefined;
+    }
+  }
+
+  return renderBounds;
+};
+
+const createRenderPassesBounds = (poisLength: number) => {
+  const res: (LatLngBounds | undefined)[][] = Array(RENDER_SET_UP.length);
+  for (let i = 0; i < res.length; i++) {
+    res[i] = Array(poisLength);
+  }
+  return res;
 };
 
 const onRender = (
@@ -85,77 +101,20 @@ const onRender = (
 
   const compiledModel = nullthrows(compiledModels[0]);
   const allPois = compiledModel.allPois.getIntersecting(tileInfo.tileBounds);
-
-  const sortedBounds = allPois
-    .sort((a, b) => a.travelTime - b.travelTime)
-    .map((p) => {
-      let boundsPerRender = [];
-
-      for (let i = 0; i < RENDER_SET_UP.length; i++) {
-        const renderer = RENDER_SET_UP[i];
-        if (p.travelTime >= renderer.maxTravelTime) {
-          // Unreachable anymore, next time maxTravelTime will get even smaller
-          break;
-        }
-        const walkingTimeUnconstrained = renderer.maxTravelTime - p.travelTime;
-        const walkingTime = MAX_WALKING_TIME(walkingTimeUnconstrained);
-        const newBounds = p.bounds
-          .getCenter()
-          .toBounds(2 * timeToDistance(walkingTime));
-        if (!newBounds.intersects(tileInfo.tileBounds)) {
-          // The point is no longer renderable within this tile
-          // over the next passes as maxTravelTime gets lower
-          // there will be no more chances to render this tile
-          break;
-        }
-        boundsPerRender[i] = newBounds;
-      }
-
-      // Second pass, remove bounds that overlap with next pass
-      // as these are no-ops
-      for (let i = 0; i < RENDER_SET_UP.length - 1; i++) {
-        const bounds = boundsPerRender[i];
-        const nextBounds = boundsPerRender[i + 1];
-        if (
-          bounds !== undefined &&
-          nextBounds !== undefined &&
-          nextBounds.contains(bounds)
-        ) {
-          boundsPerRender[i] = undefined;
-        }
-      }
-      return { travelTime: p.travelTime, boundsPerRender };
-    });
+  const rendersPassesPerBound = createRenderPassesBounds(allPois.length);
+  allPois.forEach((p, pid) =>
+    calculateBoundsForPoi(p, tileInfo.tileBounds).forEach(
+      (rp, rid) => (rendersPassesPerBound[rid][pid] = rp)
+    )
+  );
 
   d.fill("rgba(50,50,50,0.4)");
-
-  for (let rid = 0; rid < RENDER_SET_UP.length; rid++) {
-    const renderer = RENDER_SET_UP[rid];
+  RENDER_SET_UP.forEach((renderer, rid) => {
     const color = renderer.color;
-
-    for (let bid = 0; bid < sortedBounds.length; bid++) {
-      const bound = sortedBounds[bid];
-
-      if (bound.travelTime >= renderer.maxTravelTime) {
-        // Unreachable anymore, next time maxTravelTime will get even smaller
-        break;
-      }
-
-      const bounds = bound.boundsPerRender[rid];
-      if (bounds === undefined) {
-        continue;
-      }
-
-      if (boundsFilledScreenCompletely(bounds, tileInfo.tileBounds)) {
-        // Current point would fill the screen, just do it directly
-        d.cutOutFill(color);
-        // No more points can be rendered for this pass
-        break;
-      }
-
-      d.drawCircle(bounds, color);
-    }
-  }
+    rendersPassesPerBound[rid]
+      .filter((bound) => bound !== undefined)
+      .forEach((bounds) => d.drawCircle(nullthrows(bounds), color));
+  });
 };
 
 const setMaxTravelTime = RENDER_SET_UP[0].maxTravelTime;
